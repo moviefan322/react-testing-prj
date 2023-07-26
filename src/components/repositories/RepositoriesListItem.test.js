3e928c0799d818b786ec3dbeef167772d849444d
moviefan322 <philman202@yahoo.com>
2023-07-25 22:19:32 -0400

    adds test for language icon

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -5,7 +5,7 @@ import RepositoriesListItem from "./RepositoriesListItem";
 function renderComponent() {
   const repository = {
     full_name: "facebook/react",
-    language: "JavaScript",
+    language: "Javascript",
     description: "test description",
     owner: "c0demonkey",
     name: "react",
@@ -24,10 +24,18 @@ function renderComponent() {
 test("shows a link to the github repo", async () => {
   const { repository } = renderComponent();
 
-  await screen.findByRole("img", { name: "JavaScript" });
+  await screen.findByRole("img", { name: "Javascript" });
 
   const link = screen.getByRole("link", {
     name: /github repository/i
   });
   expect(link).toHaveAttribute("href", repository.html_url);
 });
+
+test("shows a file icon with the appropriate icon", async () => {
+  renderComponent();
+
+  const icon = await screen.findByRole("img", { name: "Javascript" });
+
+  expect(icon).toHaveClass("js-icon");
+})
